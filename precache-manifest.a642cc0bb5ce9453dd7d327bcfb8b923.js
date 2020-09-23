4726127c1289a236d05345b3db1f5f605c3f1fdb
Tofik <smilokiller@mail.ru>
2020-09-23 20:03:25 +0300

    Updates

---
+++ precache-manifest.a642cc0bb5ce9453dd7d327bcfb8b923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2436324ceecbaf34495c094892df1bbf",
+    "url": "/goit-markup-react-7/index.html"
+  },
+  {
+    "revision": "f171ea2e4b39b1b2ebfa",
+    "url": "/goit-markup-react-7/static/css/main.e26ea59b.chunk.css"
+  },
+  {
+    "revision": "e7a07f977d98b39b82c8",
+    "url": "/goit-markup-react-7/static/js/2.26bfca5f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-markup-react-7/static/js/2.26bfca5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f171ea2e4b39b1b2ebfa",
+    "url": "/goit-markup-react-7/static/js/main.c4561440.chunk.js"
+  },
+  {
+    "revision": "26bb6772f18e2d5fac61",
+    "url": "/goit-markup-react-7/static/js/runtime-main.ee3a602d.js"
+  }
+]);
